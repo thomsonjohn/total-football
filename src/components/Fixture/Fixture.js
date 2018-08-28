65dca435c1de248987c93a65e2be54102bd8db7a
John Thomson <john.thomson@fanduel.com>
2018-08-28 21:49:38 +0100

    Create and style fixture component

--- src/components/Fixture/Fixture.js
+++ src/components/Fixture/Fixture.js
@@ -1,14 +1,32 @@
 import React from "react";
 
+import "./Fixture.css";
+
+import defaultBadge from "../../assets/img/shield.png";
+
 const Fixture = props => {
   const { match, index } = props;
   return (
     <div className="fixture" key={index}>
-      <div className="fixture__team">
-        <p>{match.homeTeam.name}</p>
+      <div className="fixture__badge-container">
+        <img className="fixture__badge" src={defaultBadge} alt="badge" />
       </div>
-      <div className="fixture__team">
-        <p>{match.awayTeam.name}</p>
+      <div className="fixture__team fixture__team--home">
+        <p className="fixture__team-name">{match.homeTeam.name}</p>
+      </div>
+      <div className="fixture__score">
+        <div className="fixture__goals">
+          <p>
+            <span>{match.score.fullTime.homeTeam}</span>:
+            <span>{match.score.fullTime.awayTeam}</span>
+          </p>
+        </div>
+      </div>
+      <div className="fixture__team fixture__team--away">
+        <p className="fixture__team-name">{match.awayTeam.name}</p>
+      </div>
+      <div className="fixture__badge-container">
+        <img className="fixture__badge" src={defaultBadge} alt="badge" />
       </div>
     </div>
   );
