4a0e0431167226a733577a5b8f2e48fb37a4c348
John Thomson <john.thomson@fanduel.com>
2018-09-04 12:08:35 +0100

    Add logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,9 +2,12 @@ import React from "react";
 
 import "./Header.css";
 
+import logo from "../../assets/img/football-badge.png";
+
 const Header = () => {
   return (
     <header>
+      <img className="header__logo" src={logo} alt="logo" />
       <h1 className="header__title">Total Football</h1>
     </header>
   );
